f36ae0cea33b45d98691dbd6e4b8f4a1598dbb02
Sathyajith Bhat <sathya@sathyasays.com>
2018-12-25 19:04:09 +0530

    add save to trello card feature

--- kittusbot/bot.py
+++ kittusbot/bot.py
@@ -1,6 +1,7 @@
 from base.scaffold import log, telegram_token
+from trello.trello import add_to_trello_list
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+from requests import codes
 
 def start(bot, update):
     log.info('Got start command')
@@ -11,18 +12,18 @@ def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 
-def towatch(bot, update, args):
-    bot.send_message(chat_id=update.message.chat_id, text=args)
+def to_watch(bot, update, args):
+    message, status_code = add_to_trello_list('to_watch', 'card', update, args)
+    bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
 def main(updater):
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    echo_handler = MessageHandler(Filters.text, echo)
-    dispatcher.add_handler(echo_handler)
-    towatch_handler = CommandHandler('towatch', towatch, pass_args=True)
-    dispatcher.add_handler(towatch_handler)
+
+    to_watch_handler = CommandHandler('towatch', to_watch, pass_args=True)
+    dispatcher.add_handler(to_watch_handler)
     log.info('Running poller')
     updater.start_polling()
 
